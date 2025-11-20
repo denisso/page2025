d1f8003f9e1580cf16d16ea94ccd65eb5d43cacc
Denis (mr_dramm) <like-nix@mail.ru>
2025-11-20 23:02:54 +0300

    feat: добавлены posts страницы

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,10 +1,12 @@
-
+import { ListPosts } from "@/widgets/ListPosts";
 
 export default function Page() {
   return (
     <div>
       <h1>posts</h1>
-
+      <div>
+        <ListPosts />
+      </div>
     </div>
   );
 }
