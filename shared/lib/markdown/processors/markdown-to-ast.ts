548573ecc2d20811b466bac817a47e3170201c8f
Denis (mr_dramm) <like-nix@mail.ru>
2025-12-09 23:06:14 +0300

    feat: очистка markdown ast от ненужных тегов и свойств

--- shared/lib/markdown/processors/markdown-to-ast.ts
+++ shared/lib/markdown/processors/markdown-to-ast.ts
@@ -1,18 +1,29 @@
 /**
- * unified процессор 
- * генерирует ast дерево из markdown 
+ * unified процессор
+ * генерирует ast дерево из markdown
  */
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
 import { rehypeAddIdToHead } from "../plugins/rehypeAddIdToHead";
+import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
 
-export async function parseMarkdownToAST(markdown: string) {
+export async function transformMarkdownToAST(markdown: string) {
   const processor = unified()
     .use(remarkParse)
     .use(remarkGfm)
     .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeSanitize, {
+      ...defaultSchema,
+      tagNames: [...(defaultSchema.tagNames || []), "section", "iframe"],
+      attributes: {
+        ...defaultSchema.attributes,
+        "*": ["className"],
+        iframe: ["width", "height", "src", "title", "allow", "allowfullscreen"],
+        a: ["href", "rel", "target"],
+      },
+    })
     .use(rehypeAddIdToHead);
 
   const ast = processor.parse(markdown);
