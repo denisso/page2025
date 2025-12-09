e7dfee94bc8cc04063db9753269f3d57c18c03ce
Denis (mr_dramm) <like-nix@mail.ru>
2025-12-09 23:00:32 +0300

    feat: обработка загрузки контента поста с помощью suspense

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -2,33 +2,49 @@ import { getPostById } from "@/entities/post";
 import { notFound } from "next/navigation";
 import type { Post } from "@/entities/post";
 import Link from "next/link";
+import { Root } from "hast";
+import { transformMarkdownToAST } from "@/shared/lib/markdown";
+import { MarkdownPrivew } from "@/shared/ui/Markdown/Preview";
+import { Suspense } from "react";
 
 interface PageProps {
   params: {
     slug: string;
   };
 }
-
-export default async function PostPage({ params }: PageProps) {
-  let post: null | Post = null;
-  try {
-    post = await getPostById((await params).slug);
-  } catch {}
+const testAWiwatPromise = async () => new Promise(resolve=>{
+  setTimeout(resolve, 10000)
+})
+const Post = async ({ slug }: { slug: string }) => {
+  await testAWiwatPromise()
+  const post: null | Post = await getPostById(slug).catch(() => null);
 
   if (!post) {
     return notFound();
   }
 
+  const ast: Root = await transformMarkdownToAST(post.fields.body);
   return (
     <article className="post">
-      <Link href="/posts/1" className="back-link">
-        ← Назад к постам
-      </Link>
       <header>
         <h1>{post.fields.title}</h1>
       </header>
       <div className="post-content">{post.fields.description}</div>
+      <MarkdownPrivew ast={ast} />
     </article>
+  );
+};
+
+export default async function PostPage({ params }: PageProps) {
+  return (
+    <div className="container">
+      <Link href="/posts/1" className="back-link">
+        ← Назад к постам
+      </Link>
+      <Suspense fallback={<div className="loading">Loading post...</div>}>
+        <Post slug={(await params).slug} />
+      </Suspense>
+    </div>
   );
 }
 
