c8cb3e2a5d498d2a3a01e3e83a102183a9e8ea34
Denis (mr_dramm) <like-nix@mail.ru>
2025-12-14 19:10:36 +0300

    docs: добавленые комментарии генератору уникального id для заголовков статей

--- shared/lib/markdown/helpers/genUniqId.ts
+++ shared/lib/markdown/helpers/genUniqId.ts
@@ -1,10 +1,17 @@
 /**
- * генератор уникального id для заголовков статей
+ * Генератор уникального id для заголовков статей
  */
 import { translite } from "@/shared/lib/translite";
 
+/**
+ * Фабрика для генератора уникального id
+ * @returns 
+ */
 export const factoryGenUniqId = () => {
   const usedIds: Set<string> = new Set();
+  /**
+   * Генерирует уникальный id
+   */
   return (title: string) => {
     let id = translite(title);
     let counter: number = 1;
